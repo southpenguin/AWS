1c44deccc8ff51c4ced1ba18e52bc48bf6481aa2
southpenguin <southpenguin@gmail.com>
2014-10-23 16:08:40 -0400

    Create EBS.java

---
+++ EBS.java
@@ -0,0 +1,46 @@
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.AttachVolumeRequest;
+import com.amazonaws.services.ec2.model.CreateVolumeRequest;
+import com.amazonaws.services.ec2.model.CreateVolumeResult;
+
+
+
+public class EBS {
+	
+    static AmazonEC2 ec2;
+	public static void main(String[] args) throws Exception {
+
+		try {
+			AWSCredentials credentials = new PropertiesCredentials(
+		   			 AwsSample.class.getResourceAsStream("AwsCredentials.properties"));
+			AmazonEC2Client ec2 = new AmazonEC2Client(credentials);
+				
+			CreateVolumeRequest createVolumeRequest = new CreateVolumeRequest()
+		   	    .withAvailabilityZone("us-east-1c")			//Zone of my current instance running on
+		   	    .withSize(1); 
+
+			CreateVolumeResult createVolumeResult = ec2.createVolume(createVolumeRequest);
+
+			AttachVolumeRequest attachRequest = new AttachVolumeRequest()
+		   	    .withVolumeId(createVolumeResult.getVolume().getVolumeId())
+		   	    .withInstanceId("i-9a195171")	//This is my running instance ID
+		   	    .withDevice("/dev/sdf");		//Regular volume type
+
+			Thread.sleep(3000);					//Need to wait for volume to initialize
+
+			ec2.attachVolume(attachRequest);
+			
+			
+        } catch (AmazonServiceException ase) {
+                System.out.println("Caught Exception: " + ase.getMessage());
+                System.out.println("Reponse Status Code: " + ase.getStatusCode());
+                System.out.println("Error Code: " + ase.getErrorCode());
+                System.out.println("Request ID: " + ase.getRequestId());
+        } 
+       
+    }
+}
