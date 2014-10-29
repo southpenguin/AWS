486ab4873c977eb88ef34810394c9d0bf1c66df6
southpenguin <southpenguin@gmail.com>
2014-10-29 16:04:36 -0400

    Create TwitterAPI.php

---
+++ User/TwitterAPI.php
@@ -0,0 +1,11 @@
+//string s = time();
+
+Authorization: 
+        OAuth oauth_consumer_key="iY6TR3WAtPBmNuk1AFC2jma35", 
+              //oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg", 
+              oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D", 
+              oauth_signature_method="HMAC-SHA1", 
+              oauth_timestamp=s
+                //"1318622958", 
+              oauth_token="93842810-nzmCHsmiAD3mQ5XGw9t4C6YafVXZhSUzEJV01r3fK", 
+              oauth_version="1.0"
